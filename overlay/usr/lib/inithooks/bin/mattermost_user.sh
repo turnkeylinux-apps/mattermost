cee4b9a2979ce9e9bc8c819c07c96737e5bea7dc
Rik Goldman <rikgoldman@gmail.com>
2016-05-16 06:01:24 -0400

    set teamname, mess with services in init.d

--- overlay/usr/lib/inithooks/bin/mattermost_user.sh
+++ overlay/usr/lib/inithooks/bin/mattermost_user.sh
@@ -4,7 +4,8 @@ platform="/opt/mattermost/bin"
 username="$1"
 email="$2"
 password="$3"
+teamname="$4"
 
-cd $platform && ./platform -create_user -email="$email" -password="$password" -username="$username"
+nohup (cd $platform && ./platform -create_user -email="$email" -password="$password" -username="$username" -team_name="$4")
 
-cd $platform && ./platform -assign_role -email="$email" role="system_admin"
+nohup (cd $platform && ./platform -assign_role -email="$email" role="system_admin")
