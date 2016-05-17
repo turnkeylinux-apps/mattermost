facaa259cfddd93e127e5561125a969112e180dc
Rik Goldman <rikgoldman@gmail.com>
2016-05-16 22:26:18 -0400

    finally

--- overlay/usr/lib/inithooks/bin/mattermost_user.sh
+++ overlay/usr/lib/inithooks/bin/mattermost_user.sh
@@ -6,13 +6,28 @@ password="$2"
 email="$3"
 teamname="$4"
 
-if [ -e "$platform/platform" ]; then
+function create ()
+{
+	cat << EOF	
+$platform/platform -create_user -email="$email" -password="$password" -username="$username"
+EOF
+	cd $platform && ./platform -create_user -email="$email" -password="$password" -username="$username"
 
-	cat <<EOF
-$platform/platform -create_user -team_name="$teamname" -email="$email" -password="$password" -username="username"
+}
+function assign ()
+{
+	cat << EOF
 $platform/platform -assign_role -email="$email" -role="system_admin"
 EOF
-exit 0
+	cd $platform && ./platform -assign_role -email="$email" -role="system_admin"
+
+}
+
+if [ -e "$platform/platform" ]; then
+	create $platform $email $password $username
+	assign $platform $email
+	exit 0
 else:
+	echo "There was a problem"
 	exit 1
 fi
