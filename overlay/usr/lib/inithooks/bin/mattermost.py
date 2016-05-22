c8431aff4ff194c1ea72ca97731beda8c99b18af
Jeremy Davis <jeremy@turnkeylinux.org>
2016-05-22 14:52:12 +1000

    refactored initscripts to update admin password

--- overlay/usr/lib/inithooks/bin/mattermost.py
+++ overlay/usr/lib/inithooks/bin/mattermost.py
@@ -3,7 +3,6 @@
 Option:
     --password=     unless provided, will ask interactively
     --email=    unless provided, will ask interactively
-    --username= unless provided, will ask interactively
 """
 
 import re
@@ -11,15 +10,9 @@ import sys
 import getopt
 import inithooks_cache
 
-#import crypt
-#import random
-#import hashlib
-
 from dialog_wrapper import Dialog
-#from pgsqlconf import PostgreSQL
-#from subprocess import call
-import subprocess
-import shlex
+from pgsqlconf import PostgreSQL
+import bcrypt
 
 def usage(s=None):
     if s:
@@ -31,13 +24,12 @@ def usage(s=None):
 def main():
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
-                                       ['help', 'password=', 'email=', 'username='])
+                                       ['help', 'password=', 'email='])
     except getopt.GetoptError, e:
         usage(e)
 
     password = ""
     email = ""
-    username = ""
 
     for opt, val in opts:
         if opt in ('-h', '--help'):
@@ -46,56 +38,28 @@ def main():
             email = val
         elif opt == '--password':
             password = val
-        elif opt == '--username':
-            username = val
-
-    if not username:
-        d=Dialog('Turnkey Linux - First boot configuration')
-        username = d.get_input(
-            "Mattermost Admin User Name",
-            "Enter a username for the Mattermost administrator.", "admin")
-
 
     if not email:
-        #if 'd' not in locals():
+        if 'd' not in locals():
+            d = Dialog('TurnKey Linux - First boot configuration')
 
         email = d.get_email(
             "Mattermost Administrator's Email",
-            "Enter email address for the Mattermost 'admin' user.",
+            "Enter email address for Mattermost 'admin' account.",
             "admin@example.com")
 
+    inithooks_cache.write('APP_EMAIL', email)
 
     if not password:
         password = d.get_password(
             "Mattermost Admin Password",
-            "Enter new password for the Mattermost administrator's  account.")
+            "Enter new password for Mattermost 'admin' account.")
 
-    """if not teamname:
-        d = Dialog('TurnKey Linux - First boot configuration')
-        teamname = d.get_input(
-            "Initial team name",
-            "Enter a name for the first Mattermost team","Initial_Team")"""
+    salt = bcrypt.gensalt()
+    hashpass = bcrypt.hashpw(password, salt)
 
-    """
-    create_user = "/opt/mattermost/bin/platform -create_user -team_name=\'%s\' -email=\'%s\' -password=\'%s\' -username=\'%s\'" % (teamname, email, password, username)
-    args = shlex.split(create_user)
-    p = subprocess.Popen(args)
-    role = "/opt/mattermost/bin/platform -assign_role -email=\'%s\' role='system_admin'" % (email)
-    args = shlex.split(role)
-    p = subprocess.Popen(args)
-    """
-
-
-    bashcommand = "/usr/lib/inithooks/bin/mattermost_user.sh %s %s %s &>/dev/null" % (username, password, email)
-    pieces = shlex.split(bashcommand)
-    subprocess.call(pieces)
-
-
-
-    inithooks_cache.write('APP_EMAIL', email)
-
-
-
+    p = PostgreSQL(database='mattermost')
+    p.execute('UPDATE users SET password=\'%s\' WHERE username=\'admin\';' % hashpass)
 
 if __name__ == "__main__":
     main()
